a5b9f1725da8524acbd59c5f8c6a812b0d609337
Stefan Mijucic <fanna993@gmail.com>
2019-08-12 16:32:28 +0200

    Add first version of demo

---
+++ index.js
@@ -0,0 +1,48 @@
+import { VctrApi } from "https://www.vectary.com/embed/viewer/v1/scripts/api/api.js";
+
+async function run() {
+    console.log("Example script running..");
+
+    function errHandler(err) {
+        console.log("API error", err);
+    }
+
+    async function onReady() {
+        console.log("API ready..");
+
+        try {            
+            const changeColor = async (color) => {
+                const chair = await vctrApi.getObjectByName("chair_original");
+                for(let i = 0; i < chair.childrenNames.length; i++) {
+                    const childName = chair.childrenNames[i];
+                    const child = await vctrApi.getObjectByName(childName);
+                    await vctrApi.updateMaterial(child.material, color);
+                }
+            }
+
+            console.log(await vctrApi.getObjects());
+            document.getElementById("red").addEventListener("click", () => {
+                const color = { "color": "#7a0a02"};
+                changeColor(color);
+            });
+
+            document.getElementById("black").addEventListener("click", () => {
+                const color = { "color": "#000000"};
+                changeColor(color);
+            });
+
+        } catch (e) {
+            errHandler(e);
+        }
+
+    }
+    const vctrApi = new VctrApi("d9546c47-cc37-43c7-a335-b69ea054de90", errHandler);
+    try {
+        await vctrApi.init();
+        onReady();
+    } catch (e) {
+        errHandler(e);
+    }
+}
+
+run();
